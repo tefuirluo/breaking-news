0d93a20544febc94bc222637e7c1ce9aa41569dc
tefuirluo <tefuirluo@163.com>
2022-10-14 20:01:38 +0800

    获取用户的信息

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <!--一级对象路由规则对象组件的挂载点-->
+  <!-- 一级对象路由规则对象组件的挂载点 -->
   <router-view></router-view>
 </template>
 
 <script>
-
+// import { getUserInfoAPI } from '@/api'
 export default {
-
+  /* * 测试代码
+    created () {
+      getUserInfoAPI().then(res => {
+        console.log(res)
+      })
+    }
+  */
 }
 
 </script>
