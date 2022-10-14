3e5464301f72b057ab23fb735ff076461822999f
tefuirluo <tefuirluo@163.com>
2022-10-14 17:05:19 +0800

    vuex 本地持久化配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -19,5 +19,12 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [
+    // 注入持久化插件
+    createPersistedState()
+  ]
 })
+
+// * vuex 默认保存在内存中, 刷洗之后所有的值会回归初始化, 无法做到持久化存储
+// 借助 yarn add vuex-persistedstate@3.2.1 这个包可以使 vuex 做到持久化存储
