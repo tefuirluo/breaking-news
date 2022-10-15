b1c91ad84ddcf9e4a8e355ce7bf42dcd75819e1b
tefuirluo <tefuirluo@163.com>
2022-10-15 22:06:08 +0800

    文章分类 => 添加分类 => 表单准备 + 对话框关闭重置表单

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -20,12 +20,22 @@
     </el-card>
     <!--添加文章分类 => 对话框-->
     <el-dialog
-      title="提示"
+      title="添加文章分类"
       :visible.sync="dialogVisible"
-      width="30%">
-      <span>这是一段信息</span>
+      width="30%"
+      @close="dialogCloseFn"
+    >
+      <!-- 添加的表单 -->
+      <el-form :model="addForm" :rules="addRules" ref="addRef" label-width="85px">
+        <el-form-item label="分类名称" prop="cate_name">
+          <el-input v-model="addForm.cate_name" minlength="1" maxlength="10"></el-input>
+        </el-form-item>
+        <el-form-item label="分类别名" prop="cate_alias">
+          <el-input v-model="addForm.cate_alias" minlength="1" maxlength="15"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="canceFn">取 消</el-button>
+        <el-button @click="cancelFn">取 消</el-button>
         <el-button type="primary" @click="confirmFn">确 定</el-button>
       </span>
     </el-dialog>
@@ -43,7 +53,21 @@ export default {
   data () {
     return {
       cateList: [], // 文章分类列表
-      dialogVisible: false
+      dialogVisible: false,
+      addForm: { // 添加表单的数据对象
+        cate_name: '',
+        cate_alias: ''
+      },
+      addRules: { // 添加表单的验证规则对象
+        cate_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { pattern: /^\S{1,10}$/, message: '分类名必须是1-10位的非空字符', trigger: 'blur' }
+        ],
+        cate_alias: [
+          { required: true, message: '请输入分类别名', trigger: 'blur' },
+          { pattern: /^[a-zA-Z0-9]{1,15}$/, message: '分类别名必须是1-15位的字母数字', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -57,11 +81,14 @@ export default {
     addCateShowDialogBtnFn () {
       this.dialogVisible = true
     },
-    canceFn () {
+    cancelFn () {
       this.dialogVisible = false
     },
     confirmFn () {
       this.dialogVisible = false
+    },
+    dialogCloseFn () {
+      this.$refs.addRef.resetFields()
     }
   }
 }
