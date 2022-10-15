bfe2c5fbd263cfd26e8e766e5145965bdeb6c5ec
tefuirluo <tefuirluo@163.com>
2022-10-15 17:21:10 +0800

    个人中心 => 基本资料 => 重置按钮实现

--- src/views/user/userInfo.vue
+++ src/views/user/userInfo.vue
@@ -25,7 +25,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitFn">提交修改</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="resetFn">重置</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -56,6 +56,7 @@ export default {
     }
   },
   methods: {
+    // 提交修改 => 点击事件
     submitFn () {
       this.$refs.userFormRef.validate(async valid => {
         if (valid) {
@@ -67,12 +68,19 @@ export default {
           // 更新用户信息成功, 刷新 vuex 中的数据
           this.$message.success('更新成功!')
           // 重新让 vuex 获取最新的用户数据
-          this.$store.dispatch('initUserInfo')
+          this.$store.dispatch('getUserInfoActions')
         } else {
           // 未通过校验
           return false
         }
       })
+    },
+    // 重置按钮 => 点击事件
+    resetFn () {
+      this.userForm.nickname = ''
+      this.userForm.email = ''
+      // el-form 提供了一个重置表单 => 能重置报错提示
+      this.$refs.userFormRef.resetFields()
     }
   }
 }
