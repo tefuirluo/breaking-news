b3b17256ee42514febd53571cef11b978d3d9439
tefuirluo <tefuirluo@163.com>
2022-10-15 21:33:54 +0800

    文章分类 => 添加分类 => 对话框准备

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
+    <!--预览文章分类-->
     <el-card class="box-card">
       <div slot="header" class="clearfix header-box">
         <span>文章分类</span>
-        <el-button type="primary" size="mini">添加分类</el-button>
+        <el-button type="primary" size="mini" @click="addCateShowDialogBtnFn">添加分类</el-button>
       </div>
       <!--分类表格数据-->
       <el-table :data="cateList" style="width: 100%" border stripe>
@@ -17,6 +18,17 @@
         </el-table-column>
       </el-table>
     </el-card>
+    <!--添加文章分类 => 对话框-->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="canceFn">取 消</el-button>
+        <el-button type="primary" @click="confirmFn">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -30,7 +42,8 @@ export default {
   },
   data () {
     return {
-      cateList: [] // 文章分类列表
+      cateList: [], // 文章分类列表
+      dialogVisible: false
     }
   },
   methods: {
@@ -39,6 +52,16 @@ export default {
       const res = await getArtCateListAPI()
       console.log(res)
       this.cateList = res.data.data
+    },
+    // 添加分类按钮 => 点击事件 => 对话框出现
+    addCateShowDialogBtnFn () {
+      this.dialogVisible = true
+    },
+    canceFn () {
+      this.dialogVisible = false
+    },
+    confirmFn () {
+      this.dialogVisible = false
     }
   }
 }
