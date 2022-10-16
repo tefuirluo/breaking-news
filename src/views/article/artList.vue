989f25f3f3805f5b744c320e35f607d517499e37
tefuirluo <tefuirluo@163.com>
2022-10-16 11:54:08 +0800

    发表文章 => 对话框准备

--- src/views/article/artList.vue
+++ src/views/article/artList.vue
@@ -24,8 +24,12 @@
             <el-button type="info" size="small">重置</el-button>
           </el-form-item>
         </el-form>
+        <!-- 发表文章的 Dialog 对话框 -->
+        <el-dialog title="发表文章" :visible.sync="pubDialogVisible" fullscreen :before-close="handleClose">
+          <span>这是一段信息</span>
+        </el-dialog>
         <!-- 发表文章的按钮 -->
-        <el-button type="primary" size="small" class="btn-pub">发表文章</el-button>
+        <el-button type="primary" size="small" class="btn-pub" @click="showPubDialogFn">发表文章</el-button>
       </div>
 
       <!-- 文章表格区域 -->
@@ -46,7 +50,28 @@ export default {
         pagesize: 2,
         cate_id: '',
         state: ''
-      }
+      },
+      pubDialogVisible: false // 控制发表文章对话框的显示与隐藏
+    }
+  },
+  methods: {
+    // 发表文章按钮->点击事件->让添加文章对话框出现
+    showPubDialogFn () {
+      this.pubDialogVisible = true
+    },
+    // 对话框关闭前的回调
+    async handleClose (done) { // done 的作用: 调用才会放行让对话框关闭
+      // 询问用户是否确认关闭对话框
+      const confirmResult = await this.$confirm('此操作将导致文章信息丢失, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      
+      // 取消了关闭-阻止住, 什么都不干
+      if (confirmResult === 'cancel') return
+      // 确认关闭
+      done()
     }
   }
 }
