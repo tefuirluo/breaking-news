357d55724f9a65419c65bfeff1b63fef8a572196
tefuirluo <tefuirluo@163.com>
2022-10-16 11:16:42 +0800

    文章列表 => 组件 + 路由

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,10 @@ const routes = [
       {
         path: 'art-cate',
         component: () => import('@/views/article/artCate')
+      },
+      {
+        path: 'art-list',
+        component: () => import('@/views/article/artList')
       }
     ]
   },
