9d27ac240895cb1151f1e660b7ec369c8d3a224f
tefuirluo <tefuirluo@163.com>
2022-10-17 21:50:53 +0800

    打包 => dist 瘦身

--- src/ElementUI/index.js
+++ src/ElementUI/index.js
@@ -2,7 +2,7 @@
 // 每个功能单独一个文件分开管理
 
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import ELEMENTUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+Vue.use(ELEMENTUI)
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    externals: {
+      // provide the app's title in webpack's name field, so that
+      // it can be accessed in index.html to inject the correct title.
+      // 基本格式：
+      // '包名' : '在项目中引入的名字'
+      echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      dayjs: 'dayjs',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vuex-persistedstate': 'createPersistedState'
+    }
+  }
 })
